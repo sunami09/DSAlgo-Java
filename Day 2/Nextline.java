5c6b33c41fb0546dcd3d4f26c850381d20f5bb28
sunami09 <sunamidasgupta@gmail.com>
2022-02-06 18:29:59 +0530

    nextLine() problem fixed

---
+++ Day 2/Nextline.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+
+
+public class Nextline{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        int k = Integer.parseInt(in.nextLine());
+        String j = in.nextLine();
+        System.out.println(j);
+        System.out.println(k+30);
+    }
+}

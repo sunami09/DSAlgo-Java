2d3a6a1abaa56de68c21a42e032214403d23008e
sunami09 <sunamidasgupta@gmail.com>
2022-02-16 18:57:08 +0530

    Find Second Largest - Problem Code: SECLAR

---
+++ Day 8 - CodeChef Level 1/SecondLargest.java
@@ -0,0 +1,37 @@
+
+/**
+Given three distinct integers A, B and C, print the second largest number among them.
+
+Constraints
+1≤A,B,C≤10^9
+
+
+Sample Input 1:
+2
+7
+21
+Sample Output 1:
+7
+
+*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class SecondLargest {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        if (a > b && a < c || a < b && a > c) {
+            System.out.println(a);
+        } else if (b > c && b < a || b < c && b > a) {
+            System.out.println(b);
+        } else if (c > b && c < a || c < b && c > a) {
+            System.out.println(b);
+        }
+
+    }
+}

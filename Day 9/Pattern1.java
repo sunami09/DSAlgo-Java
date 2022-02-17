8aa70bfd0b8e7e06abab7794100c67892112e2a9
sunami09 <sunamidasgupta@gmail.com>
2022-02-17 15:40:16 +0530

    Pattern 1

---
+++ Day 9/Pattern1.java
@@ -0,0 +1,40 @@
+/*
+1. You are given a number n.
+2. You've to create a pattern of * and separated by tab as shown in output format.
+
+Constraints
+1 <= n <= 100
+
+Example
+
+Sample Input
+5
+
+Sample Output
+*	
+*	*	
+*	*	*	
+*	*	*	*	
+*	*	*	*	*	
+
+
+
+*/
+
+
+import java.util.*;
+public class Pattern1{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+
+    }
+}

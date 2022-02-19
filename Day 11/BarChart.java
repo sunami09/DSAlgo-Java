db5d07f329bca35bf7cd029f0f1757e0edb0dc7e
sunami09 <sunamidasgupta@gmail.com>
2022-02-19 21:37:18 +0530

    Bar Chart

--- Day 11/BarChart.java
+++ Day 11/BarChart.java
@@ -1,3 +1,17 @@
+/*
+1. You are given a number n, representing the size of array a.
+2. You are given n numbers, representing elements of array a.
+3. You are required to print a bar chart representing value of arr a.
+
+Constraints
+1 <= n <= 30
+0 <= n1, n2, .. n elements <= 10
+
+
+
+*/
+
+
 import java.io.*;
 import java.util.*;
 

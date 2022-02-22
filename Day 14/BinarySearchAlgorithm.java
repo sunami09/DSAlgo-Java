630545073381d06ca5fa30183979c04a12e0517f
sunami09 <sunamidasgupta@gmail.com>
2022-02-22 19:48:43 +0530

    Binary Search Algorithm

--- Day 14/BinarySearchAlgorithm.java
+++ Day 14/BinarySearchAlgorithm.java
@@ -2,20 +2,22 @@ import java.util.*;
 
 public class BinarySearchAlgorithm {
     public static void main(String[] args) {
-        int[] arr = {-1, 0, 3, 5, 71, 123, 129, 132, 150, 289};
-        int target = 0;
+        int[] arr = { -1, 0, 3, 5, 71, 123, 129, 132, 150, 289 };
+        int target = 123;
         int answer = findIndex(arr, target);
+        System.out.println(answer);
     }
-    static int findIndex(int[] arr, int target){
+
+    static int findIndex(int[] arr, int target) {
         int start = 0;
-        int end = arr.length-1;
-        while(start <= end){
-            int mid = start + (end-start)/2;
-            if(arr[mid]>target){
-                start = mid +1;
-            }else if(arr[mid]<target){
-                end = mid-1;
-            }else{
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target> arr[mid]) {
+                start = mid + 1;
+            } else if (target<arr[mid]) {
+                end = mid - 1;
+            } else {
                 return mid;
             }
         }

ffe2b2d7b36b61533f246f4decec9c4c23b1cdb2
sunami09 <sunamidasgupta@gmail.com>
2022-02-22 19:30:58 +0530

    Binary Search Algoritm

---
+++ Day 14/BinarySearchAlgorithm.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class BinarySearchAlgorithm {
+    public static void main(String[] args) {
+        int[] arr = {-1, 0, 3, 5, 71, 123, 129, 132, 150, 289};
+        int target = 0;
+        int answer = findIndex(arr, target);
+    }
+    static int findIndex(int[] arr, int target){
+        int index = -1;
+        
+        return index;
+    }
+}
